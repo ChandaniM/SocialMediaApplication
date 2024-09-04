b70b99151db6b39e43600dec93951903117ab794
ChandaniM <chandanimourya5@gmail.com>
2024-09-04 11:04:30 +0530

    Basic server setup

--- BE/index.js
+++ BE/index.js
@@ -1,4 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { Pool } = require("pg");
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
